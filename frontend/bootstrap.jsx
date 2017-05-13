1b209620de6762cc831bf3d3f84ee335ddb47704
Jakub Niewczas <niewczas.jakub@gmail.com>
2017-05-13 15:28:04 +0200

    rename variable

--- frontend/bootstrap.jsx
+++ frontend/bootstrap.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CurrentState from 'lib/current-state.js';
-import ServerEventStorage from 'lib/server-event-storage.js';
+import EventStorage from 'lib/server-event-storage.js';
 import Board from 'components/Board/Board.jsx';
 import 'assets/stylesheets/style.scss';
 
-const eventStorage = new ServerEventStorage();
+const eventStorage = new EventStorage();
 const stateManager = new CurrentState({
   eventSource: eventStorage
 });
