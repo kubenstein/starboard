04afbe2bffd20fc8324ec6329c2d390731abfe2d
Jakub Niewczas <niewczas.jakub@gmail.com>
2017-05-23 20:36:52 +0200

    labels on card

--- src/frontend/components/Card/Card.jsx
+++ src/frontend/components/Card/Card.jsx
@@ -5,6 +5,7 @@ import 'components/Card/card.scss';
 export default class Card extends React.Component {
   constructor(props) {
     super(props);
+    this.card = this.props.data;
     this.stateManager = this.props.stateManager;
     this.state = {
       detailsOpened: false,
@@ -24,8 +25,7 @@ export default class Card extends React.Component {
   }
 
   render() {
-    const cardData = this.props.data;
-    const { title, id } = cardData;
+    const { labels, title, id } = this.card;
     const { detailsOpened } = this.state;
     return (
       <div
@@ -33,14 +33,25 @@ export default class Card extends React.Component {
         data-DND-data-card-id={id}
         onClick={() => { this.openDetails(); }}
       >
-        <h1 className="title card-DND-handler">{title}</h1>
+        <div className="title card-DND-handler">
+          <ul className="labels">
+            { (labels || []).map(label =>
+              <li
+                key={label}
+                className="label"
+                style={{ backgroundColor: label }}
+              />
+            )}
+          </ul>
+          {title}
+        </div>
         {detailsOpened ?
           <div
             className="card-details-full-screen-wrapper"
             onClick={(e) => { this.closeDetails(e); }}
             ref={(r) => { this.dismissOverlayElement = r; }}
           >
-            <CardDetails data={cardData} stateManager={this.stateManager} />
+            <CardDetails data={this.card} stateManager={this.stateManager} />
           </div>
         : ''}
       </div>
