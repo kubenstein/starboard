b9162944658065c3cfc6ba6afff88747f80bc25e
Jakub Niewczas <niewczas.jakub@gmail.com>
2017-05-26 22:34:45 +0200

    clear files from removed column

--- src/lib/clean-files-usecase.js
+++ src/lib/clean-files-usecase.js
@@ -1,6 +1,11 @@
 import fs from 'fs';
 import CommentsRepository from './comments-repository.js';
-import { cardRemovedEventType, commentRemovedEventType } from './event-definitions.js';
+import CardsRepository from './cards-repository.js';
+import {
+  columnRemovedEventType,
+  cardRemovedEventType,
+  commentRemovedEventType
+} from './event-definitions.js';
 
 export default class CleanFilesUsecase {
   constructor(currentState, params) {
@@ -8,31 +13,41 @@ export default class CleanFilesUsecase {
     this.pathToStorage = params.pathToStorage;
     this.fileNamePrefix = params.fileNamePrefix;
     this.commentsRepo = new CommentsRepository(this.currentState);
+    this.cardsRepo = new CardsRepository(this.currentState);
   }
 
   cleanWhenNeeded(event) {
     const t = event.type;
-    if (t === commentRemovedEventType) return this.removeAllFilesFromCommentByCommentId(event.data.commentId);
-    if (t === cardRemovedEventType) return this.removeAllFilesFromCardComments(event.data.cardId);
+    if (t === columnRemovedEventType) return this.removeColumnRelatedFiles(event.data.columnId);
+    if (t === cardRemovedEventType) return this.removeCardRelatedFiles(event.data.cardId);
+    if (t === commentRemovedEventType) return this.removeCommentRelatedFiles(event.data.commentId);
     return Promise.resolve();
   }
 
   // private
 
-  removeAllFilesFromCommentByCommentId(commentId) {
-    const comment = this.commentsRepo.get(commentId);
-    return this.removeAllFilesFromComment(comment);
-  }
-
-  removeAllFilesFromCardComments(cardId) {
-    const comments = this.commentsRepo.getCommentsForCard(cardId);
-    const promises = comments.map((c) => {
-      return this.removeAllFilesFromComment(c);
+  removeColumnRelatedFiles(columnId) {
+    const cards = this.cardsRepo.getCardsSortedByPosition(columnId);
+    const promises = cards.map((card) => {
+      return this.removeCardRelatedFiles(card.id);
     });
     return Promise.all(promises);
   }
 
-  removeAllFilesFromComment(comment) {
+  removeCardRelatedFiles(cardId) {
+    const comments = this.commentsRepo.getCommentsForCard(cardId);
+    const promises = comments.map((comment) => {
+      return this.removeAttachment(comment);
+    });
+    return Promise.all(promises);
+  }
+
+  removeCommentRelatedFiles(commentId) {
+    const comment = this.commentsRepo.get(commentId);
+    return this.removeAttachment(comment);
+  }
+
+  removeAttachment(comment) {
     if (!comment.attachment) return Promise.resolve();
 
     const filePublicUrl = comment.attachment.dataUrl;
