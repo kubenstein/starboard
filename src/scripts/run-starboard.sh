9b5ef832831f1b47e54579e7e3b75f3adbb924da
Jakub Niewczas <niewczas.jakub@gmail.com>
2017-10-04 23:40:51 +0900

    fix tmp path

--- src/scripts/run-starboard.sh
+++ src/scripts/run-starboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOCAL_REPO_DIR='/tmp/starboard/projects/starboard' \
+LOCAL_REPO_DIR='/tmp/starboard/projects/starboard/' \
 SYNCING_INTERVAL=60 \
 REPO_URL=$(git remote show dropbox | grep 'Fetch URL' | cut -d' ' -f5) \
 starboard
