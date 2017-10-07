77d0ec661d2a116be0ccee9612e5a6100a15be7f
Jakub Niewczas <niewczas.jakub@gmail.com>
2017-10-07 19:25:05 +0900

    transition to DI: Topbar component

--- src/frontend/components/Topbar/index.jsx
+++ src/frontend/components/Topbar/index.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import EditableInput from 'components/EditableInput';
 import SideMenu from 'components/SideMenu';
-import SettingsRepository from 'lib/repositories/settings-repository';
-import BrowserSettingsService from 'lib/services/browser-settings-service';
 import 'components/Topbar/styles.scss';
 
 export default class Topbar extends React.Component {
   static get propTypes() {
     return {
-      stateManager: PropTypes.object.isRequired,
+      deps: PropTypes.object.isRequired,
     };
   }
 
   constructor(props) {
     super(props);
-    const { stateManager } = this.props;
-    this.browserSettingsService = new BrowserSettingsService();
-    this.repo = new SettingsRepository(stateManager);
+    this.deps = this.props.deps;
+    this.browserSettingsService = this.deps.get('browserSettingsService');
+    this.repo = this.deps.get('settingsRepository');
   }
 
   updateBoardName(name) {
@@ -33,7 +31,6 @@ export default class Topbar extends React.Component {
   }
 
   render() {
-    const { stateManager } = this.props;
     const boardName = this.repo.boardName();
     this.updatePageTitle(boardName);
     return (
@@ -55,7 +52,7 @@ export default class Topbar extends React.Component {
             id="side-menu-checkbox"
           />
           <div className="side-menu-wrapper">
-            <SideMenu stateManager={stateManager} />
+            <SideMenu stateManager={this.deps.get('stateManager')} />
           </div>
         </div>
       </div>
--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -4,21 +4,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Bootstrap from 'components/Bootstrap';
 import DependencyInjector from 'lib/dependency-injector';
-import UserSessionService from 'lib/services/user-session-service';
 import CurrentState from 'lib/current-state';
 import ServerEventStorage from 'lib/eventStorages/server-event-storage';
 import ThemeStyler from 'components/Board/theme-styler';
+import UserSessionService from 'lib/services/user-session-service';
+import BrowserSettingsService from 'lib/services/browser-settings-service';
 import ColumnsRepository from 'lib/repositories/columns-repository';
 import CardsRepository from 'lib/repositories/cards-repository';
+import SettingsRepository from 'lib/repositories/settings-repository';
 
 import './assets';
 
 const deps = new DependencyInjector();
 deps
+  .set('settingsRepository', di => new SettingsRepository(di.get('stateManager')))
   .set('cardsRepository', di => new CardsRepository(di.get('stateManager')))
   .set('columnsRepository', di => new ColumnsRepository(di.get('stateManager')))
   .set('themeStyler', di => new ThemeStyler(di.get('stateManager')))
   .set('userSessionService', () => new UserSessionService())
+  .set('browserSettingsService', di => new BrowserSettingsService(di.get('stateManager')))
   .set('sessionToken', di => di.get('userSessionService').token())
   .set('eventStorage', di => new ServerEventStorage({
     token: di.get('sessionToken'),
